69e191c268925d689512c9b0a4e9ef73f148fb81
cranesandcaff <Patrick@cranesandcaffeine.com>
2014-05-15 20:07:45 -0400

    Update percentage.js

--- percentage.js
+++ percentage.js
@@ -2,7 +2,7 @@ angular.module('percentage', [])
     .filter('percentage', function () {
         return function (input) {
             var rounded = Math.round(input*10000)/100;
-            if (rounded == NaN) {
+            if (rounded.isNaN) {
                 return '';
             }
             var percentage = '' + rounded + '%';
